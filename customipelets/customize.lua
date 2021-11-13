adf82f1a498515dbc4fdc6a1f75a552d2866152c
Guido de Hek <guidodehek@hotmail.nl>
2021-11-13 14:46:47 +0100

    added existing ipelets to ipelets fork

---
+++ customipelets/customize.lua
@@ -0,0 +1,9 @@
+----------------------------------------------------------------------
+-- Customized ipelet: customize.lua
+----------------------------------------------------------------------
+prefs.max_zoom = 100
+shortcuts.ipelet_1_symbols = "Shift+S"
+shortcuts.mode_splines = "Alt+Ctrl+I"
+prefs.scroll.vertical_sign = -1
+-- Substitute with /path/to/ElectricalEngineering.isy
+prefs.styles = { "C:\\ipe\\ipe-7.2.24\\styles\\ElectricalEngineering.isy" }
